af6071a087cd24f74a1c0353e3bfe91d6c2dd57c
Rodrigo <madrid.rodrigo2000@gmail.com>
2024-10-21 14:33:49 -0300

    primer commit

--- index.js
+++ index.js
@@ -12,11 +12,5 @@ function botones(nombre, id, link) {
     return boton;
 }
 
-const botonCV = botones("Ver Cv", "boton1", "https://drive.google.com/file/d/1bwxZVZyiJWxRK9gjVEi2qxanEvs2JQCs/view");
+const botonCV = botones("cv", "boton1", "https://drive.google.com/file/d/1ppJRVQDpsdkHBoHHBpNuELwStXDxfGj7/view");
 document.getElementById("botones").appendChild(botonCV);//pone el boton en id"botones"
-
-const botonContacto = botones("Contacto", "boton2");
-document.getElementById("botones").appendChild(botonContacto);
-document.getElementById("boton2").addEventListener("click", function() {
-    window.open("index2.html", "_blank");
-});
